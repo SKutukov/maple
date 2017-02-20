4bdf2dafd4b0140c607362ee81523c9ef8b7bd19
SKutukov <skutukov96@gmail.com>
2017-02-21 02:40:39 +0300

    small changes

--- 2_sem/1.py
+++ 2_sem/1.py
@@ -17,8 +17,22 @@ x2=np.linalg.solve(A,b2)
 print x2;
 
 A_inv=np.linalg.inv(A)
-print(A_inv)
-norm1 = np.linalg.norm(A)
-norm2 = np.linalg.norm(A_inv)
-cond =norm1*norm2;
+#print(A_inv)
+norm = np.linalg.norm(A)
+norm_inv = np.linalg.norm(A_inv)
+cond =norm*norm_inv;
+print('cond:');
 print(cond);
+
+norm_b=np.linalg.norm(b1);
+#print(norm)
+#print(norm_b)
+db=1;
+dA=0.0001;
+eps_teor=(cond/(1-cond*dA/norm))*(db/norm_b+dA/norm)
+print('teoretical error:')
+print(eps_teor)
+print('error:')
+print(x1-x2)
+print('fact error:')
+print(np.linalg.norm(x1-x2)/np.linalg.norm(x1))
