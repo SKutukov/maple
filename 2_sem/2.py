ff12d28942a844f7e703920a5b5d01880251ce96
skutukov <skutukov96@gmail.com>
2017-02-28 00:59:20 +0300

    done half of 2 exisize

--- 2_sem/2.py
+++ 2_sem/2.py
@@ -1,4 +1,5 @@
 import  numpy as np
+import math
 from Norm import norm_inf 
 A= np.matrix('8.29381 0.995516 -0.560617 ; 0.995516 6.298198 0.595772 ;-0.560617 0.595772 4.997407')
 b=np.matrix(' 0.766522 ; 3.844422 ; 3.844422 ')
@@ -25,28 +26,56 @@ def transformation(A,b):
 	while(i<num_rows):
 		g[i]=b[i]/A[i,i]
 		i=i+1
-	return H,g
+	return H,np.matrix(g).T
 print "2)transformation to x=Hx+g : "
 H,g=transformation(A,b)
-print H,g
+print "H: "
+print H
+print "g: "
+print g
+print "Norm of H: "
 print norm_inf(H)
-
-
-#print ('matrix H: ') 
-#print (np.linalg.norm(H))
-
-
-#print ('x_ext: ')
-#print x_ext
-#x=np.matrix('0.4 ; 0.4 ');
-def iteration(H,x,eps):
+eps=0.001
+print "3) find k, ||x_*-x^k||<eps"
+def find_K(H,g,x_0,eps):
+	d=norm_inf(x_0)+norm_inf(g)/(1-norm_inf(H))
+	eps_d=eps/d
+	return math.ceil(np.log(eps/d)/np.log(norm_inf(H)))
+x_0=x_ext+np.matrix(' 0.2  0.2  0.2 ').T
+k_teor=find_K(H,g,x_0,eps)	
+print "k_teor: ", k_teor
+print "4) simple iteration"
+def iteration(H,g,x_0,eps):
 	dx=2*eps
+	x=x_0
+	k=0
+	h=norm_inf(H)/(1-norm_inf(H))
 	while(dx>eps):
-		print(dx)
+		#print(dx)
 		x1=x
-		x=H*x
-		dx=np.linalg.norm(x1-x)
-	return x
+		x=H*x+g
+		dx=h*norm_inf(x1-x)
+		k=k+1
+	return x,k,dx,x1
+x_iter,k_ext,dx,x_k_1= iteration(H,g,x_0,eps)
+print x_iter
+print "k_ext: ", k_ext
+print "ERROR x_ext-x_iter: "
+print x_ext-x_iter
+print "ERROR aprior: "
+def aprior(H,g,x_0,k):
+	norm=norm_inf(H)
+	d=norm_inf(x_0)+np.linalg.norm(g)/(1-norm)
+	return math.pow(norm,k)*(d)
+print aprior(H,g,x_0,k_teor)
+print "ERROR apostorior: "
+print dx
+r=math.fabs(np.linalg.eig(H)[0].max())
+x_lust=x_k_1+(x_iter-x_k_1)/(1-r)
+print "lusternick :",x_lust
+print x_lust-x_ext
 
-#print iteration(H,x,0.0000001)
-
+print "5) Zeidel"
+def zeidel(H,g,x_0,eps):
+	x=x_0
+	return x	
