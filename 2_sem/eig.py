ec40f55c234da4d6b3018e04b44a47add2c67214
skutukov <skutukov96@gmail.com>
2017-03-21 15:14:15 +0300

    update eig.py

--- 2_sem/eig.py
+++ 2_sem/eig.py
@@ -45,52 +45,50 @@ def sign(a):
 def calc_c_s(mat,row,col):
 	#prepare
 	div=mat[row,row] - mat[col,col] 
-	fi=math.atan(2*mat[row,col]*mat[row,col]/div)/2
+
+	fi=math.atan(2.0*mat[row,col]/div)/2.0
 	c=math.cos(fi)
 	s=math.sin(fi)	
 
-	#f=np.fabs(div)
-	#d=math.sqrt(div*div + 4 * mat[row,col]*mat[row,col] )
-	#c=math.sqrt(( 1 +  f/d)/2)
-	#s=sign(mat[row,col]*(mat[row,row]-mat[col,col])) * math.sqrt(( 1 -  f/d)/2)
-	#change elem
-	res=np.identity(mat.shape[0])
-	res[row,row]=c
-	res[col,col]=c
-	res[row,col]=-s
-	res[col,row]=s
-	return res
-###########################################
+#	f=np.fabs(div)
+#	d=math.sqrt(div*div + 4 * mat[row,col]*mat[row,col] )
+#	c=math.sqrt(( 1 +  f/d)/2)
+#	s=sign(mat[row,col]*(mat[row,row]-mat[col,col])) * math.sqrt(( 1 -  f/d)/2)
+	return c,s
+##########################################
 
 def Yakobi(A,eps):
 	A_k=A.copy()
-	row,col=find_max(A_k)
-	print row,col
-	if (A[row,col]<eps):
-		return A_k
-	res=calc_c_s(A,row,col)
-	A_k=mul(A_k,res)
-	A_k=mul(res.T,A_k)
-	
-	row,col=find_max(A_k)
-	print row,col
-	if (A[row,col]<eps):
-		return A_k
-	res=calc_c_s(A,row,col)
-	A_k=mul(A_k,res)
-	A_k=mul(res.T,A_k)
-
-	row,col=find_max(A_k)
-	print row,col
-	if (A[row,col]<eps):
-		return A_k
-	res=calc_c_s(A,row,col)
-	A_k=mul(A_k,res)
-	A_k=mul(res.T,A_k)
-
-	
-	#A[row,col]=A[col,row]=c*A[row,col]+s*A[col,row]
-
+	i=0
+	max_elem=2*eps
+	while (max_elem>eps):
+		row,col=find_max(A_k)
+		max_elem=A_k[row,col]
+		print max_elem
+		c,s=calc_c_s(A,row,col)
+		
+		a_i_i=A_k[row,row]
+		a_j_j=A_k[col,col]
+		a_i_j=A_k[row,col]
+		a_j_i=A_k[col,row]
+		
+		A_i=A_k[:,row]
+		A_j=A_k[:,col]
+		
+		#A_k(*,i)=.......
+		#A_k[:,row]=c*A_i+s*A_j
+		#A_k(*,j)=.......
+		#A_k[:,col]=-s*A_i+c*A_j
+		#A_k(i,i)=.......
+		#A_k[row,row]=c*c*a_i_i+2*c*s*a_i_j+s*s*a_j_j
+		#A_k(j,j)=.......
+		#A_k[col,col]=s*s*a_i_i-2*c*s*a_i_j+c*c*a_j_j
+		#A_k(i,j)=A_k(j,i)=.........		
+		A_k[row,col]=A_k[col,row]=(c*c-s*s)*a_i_j+c*s*(a_j_j-a_i_i)	
+		
+		print A_k
+		i=i+1
+		
 	return A_k
 print Yakobi(A,eps)
 def opsiteSpectr(A,lmbd):
